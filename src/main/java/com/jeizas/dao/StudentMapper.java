7969a621d5b73aed9b5d4b5aae4081b98a6432ec
jeizas <jeizas1210@gmail.com>
2017-05-26 01:11:41 +0800

    add data source for HikariCP

--- src/main/java/com/jeizas/dao/StudentMapper.java
+++ src/main/java/com/jeizas/dao/StudentMapper.java
@@ -1,10 +1,10 @@
 package com.jeizas.dao;
 
 import com.jeizas.entity.Student;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Created by jeizas on 5/25/17.
@@ -16,4 +16,16 @@ public interface StudentMapper {
 
     @Select("select * from student where id = #{id}")
     Student findById(@Param("id") int id);
+
+    @Select("select * from student")
+    List<Student> findAll();
+
+    @Insert("insert into student(name, age) values(#{name}, #{age})")
+    int save(Student student);
+
+    @Update("UPDATE student SET name=#{name} WHERE id =#{id}")
+    int update(@Param("id") int id, @Param("name") String name);
+
+    @Delete("DELETE FROM student WHERE id =#{id}")
+    void delete(int id);
 }
