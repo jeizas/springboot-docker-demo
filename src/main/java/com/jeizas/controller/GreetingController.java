442ce1c327275e1817f833e80ad8243033c0f9be
jeizas <jeizas1210@gmail.com>
2017-05-27 01:13:43 +0800

    add websocket

--- src/main/java/com/jeizas/controller/GreetingController.java
+++ src/main/java/com/jeizas/controller/GreetingController.java
@@ -1,14 +1,19 @@
 package com.jeizas.controller;
 
+import com.google.gson.Gson;
+import com.jeizas.dao.StudentMapper;
+import com.jeizas.entity.Student;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.*;
 
 /**
  * Created by jeizas on 2017/1/1.
@@ -16,38 +21,67 @@ import javax.annotation.Resource;
 @Controller
 public class GreetingController {
 
-    @Resource
+    private final static Logger logger = Logger.getLogger(GreetingController.class);
+
+    public static Set<Student> loginUsers = new HashSet<>();
+
+    @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
+    @Autowired
+    private StudentMapper studentMapper;
 
     @RequestMapping(value = "helloSocket")
     public String index() {
         return "index";
     }
 
-    @RequestMapping(value = "helloSocket1")
-    public String indexs() {
-        return "index1";
-    }
-//    /**
-//     * 接受客户端公告的socket请求
-//     * @param value
-//     */
-//    @MessageMapping("/change-notice")
-//    public void greeting(String value){
-//        //把指定的消息进行序列化，通过MessageConverter方法进行包装成一条消息，发送到指定的客户端
-//        this.simpMessagingTemplate.convertAndSend("/topic/notice", value);
-//    }
+    @GetMapping(value = "login")
+    @ResponseBody
+    public Map<String, Object> login(@RequestParam("id") Integer id) {
+        Map<String, Object> retMap = new HashMap<>();
+        if (null == id) {
+            return null;
+        }
+        Student stu = studentMapper.findById(id);
+        if (stu == null) {
+            return null;
+        }
 
-    @MessageMapping("/change-notice")
-    @SendTo("/topic/notice")
-    public String greeting(String value) {
-        System.out.println(value);
-        return value;
+        loginUsers.add(stu);
+        for (Student s : loginUsers) {
+            if (s.getId() == id) {
+                continue;
+            }
+            simpMessagingTemplate.convertAndSendToUser(String.valueOf(s.getId()), "/message", "有新用户登录");
+        }
+
+        retMap.put("list", loginUsers);
+        simpMessagingTemplate.convertAndSend("/topic/notice", retMap);
+        retMap.put("obj", stu);
+        return retMap;
     }
+
+    /**
+     * 接受客户端公告的socket请求
+     * @param value
+     */
+    @MessageMapping("/change-notice")
+    public void greeting(String value){
+        //把指定的消息进行序列化，通过MessageConverter方法进行包装成一条消息，发送到指定的客户端
+        simpMessagingTemplate.convertAndSend("/topic/notice", value);
+    }
+
+//    @MessageMapping("/change-notice")
+//    @SendTo("/topic/notice")
+//    public String greeting(String value) {
+//        logger.info(value);
+//        return value;
+//    }
 
     @MessageMapping("/message")
     @SendToUser("/message")
     public void userMessage(String msg) throws Exception {
+        logger.info(msg);
         simpMessagingTemplate.convertAndSendToUser(msg.substring(0, msg.indexOf("@") - 1), "/message", msg);
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,8 +5,21 @@ function setConnected(connected) {
     document.getElementById('conversationDiv').style.visibility = connected ? 'visible' : 'hidden';
     document.getElementById('response').innerHTML = '';
 }
-// 开启socket连接
+/**
+ * 开启socket连接
+ */
 function connect() {
+    //获取登录人ID
+    var usrId = document.getElementById('nick').value;
+    if (usrId == "") {
+        return;
+    }
+    $.ajax({url: '/login?id=' + usrId, success:function(result){
+        $('#nick').val(result.obj.name);
+
+        updateLoginUsers(result);
+    }});
+
     var socket = new SockJS('/socket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
@@ -14,16 +27,17 @@ function connect() {
         // noticeSocket();
         //method 2
         stompClient.subscribe('/topic/notice', function(greeting){
-           showGreeting(JSON.parse(greeting.body).content);
            showGreeting(greeting.body);
         });
-        stompClient.subscribe('/user/0/message', function(message){
+        stompClient.subscribe('/user/' + usrId + '/message', function(message){
             // var messageEntity = JSON.parse(message.body);
             showGreeting(message.body);
         });
     });
 }
-// 断开socket连接
+/**
+ * 断开socket连接
+ */
 function disconnect() {
     if (stompClient != null) {
         stompClient.disconnect();
@@ -31,30 +45,77 @@ function disconnect() {
     setConnected(false);
     console.log("Disconnected");
 }
-// 向‘/app/change-notice’服务端发送消息
+
+/**
+ * 向‘/app/change-notice’服务端发送消息
+ */
 function sendName() {
-    var value = document.getElementById('name').value;
-    // stompClient.send("/app/change-notice", {}, value);
-    stompClient.send("/user/1/message", {}, value);
-}
-//    connect();
+    var msg = $('#name').val();
+    var usrId = $("#usrIds").val();
 
+    if (usrId == 0) {
+        stompClient.send("/app/change-notice", {}, msg);
+    } else {
+        stompClient.send('/user/' + usrId + '/message', {}, usrId + "@" + msg);
+    }
+}
+
+/**
+ * 收到消息
+ * 方法二
+ * @param message
+ */
 function showGreeting(message) {
-    var response = document.getElementById('response');
-    var p = document.createElement('p');
-    p.style.wordWrap = 'break-word';
-    p.appendChild(document.createTextNode(message));
-    response.appendChild(p);
+    if (isJSON(message)) {
+        updateLoginUsers(message)
+    } else {
+        var response = document.getElementById('response');
+        var p = document.createElement('p');
+        p.style.wordWrap = 'break-word';
+        p.appendChild(document.createTextNode(message));
+        response.appendChild(p);
+    }
+
 }
 
+/**
+ * 收到消息
+ * 方法一
+ * @param message
+ */
 var noticeSocket = function () {
     var s = new SockJS('/socket');
     var stompClient = Stomp.over(s);
     stompClient.connect({}, function () {
         console.log('notice socket connected!');
         stompClient.subscribe('/topic/notice', function (data) {
-            console.log("receive:" + data.body);
             document.getElementById('content').innerHTML = data.body;
         });
     });
 };
+
+var updateLoginUsers = function (msg) {
+    var data = msg.list;
+    if (msg.list == undefined) {
+        // data = eval(" ' "+msg+" ' ").list;
+        data = JSON.parse(msg).list;
+    }
+    $('#usrIds').empty();
+    $('#usrIds').append($("<option>").val("0").text("select.."));
+    for (var i=0; i<data.length; i++) {
+        $('#usrIds').append($("<option>").val(data[i].id).text(data[i].name));
+    }
+}
+
+function isJSON(str) {
+    if (typeof str == 'string') {
+        try {
+            JSON.parse(str);
+            return true;
+        } catch(e) {
+            console.log(e);
+            return false;
+        }
+    }
+    console.log('It is not a string!')
+}
